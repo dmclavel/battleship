aefb813bff6edf89bd84b402b5a62b19ce46a457
Daimler Clavel <dmclavel@up.edu.ph>
2024-03-27 14:05:25 +0800

    add PWA support

---
+++ src/registerServiceWorker.ts
@@ -0,0 +1,31 @@
+const sw = 'service-worker.js';
+
+try {
+  navigator.serviceWorker
+    .register(sw)
+    .then((registration) => {
+      registration.onupdatefound = () => {
+        const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              console.log(
+                'New content is available and will be used when all ' +
+                  'tabs for this page are closed.'
+              );
+            } else {
+              console.log('Content is cached for offline use.');
+            }
+          }
+        };
+      };
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+} catch (error) {
+  console.error(error);
+}
